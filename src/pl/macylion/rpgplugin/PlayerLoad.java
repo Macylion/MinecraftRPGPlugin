8c9f876031b91be838d9f6ad6413740753f0e1d6
Maciek <macyliongra@gmail.com>
2020-06-27 13:24:17 +0200

    Add files via upload

--- src/pl/macylion/rpgplugin/PlayerLoad.java
+++ src/pl/macylion/rpgplugin/PlayerLoad.java
@@ -1,11 +1,11 @@
 package pl.macylion.rpgplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerLoad implements Listener{
@@ -28,9 +28,34 @@ public class PlayerLoad implements Listener{
 		//Max Health Points
 		if(plugin.getConfig().get(player.getUniqueId() + ".hp") == null)
 				plugin.getConfig().set(player.getUniqueId() + ".hp" ,
-						(int) plugin.getConfig().get("defaultHP"));
-		player.getPlayer().setMaxHealth((int) plugin.getConfig().get(player.getUniqueId() + ".hp"));
-		
+						(double) plugin.getConfig().get("defaultHP"));
+		//player.setMaxHealth((double) plugin.getConfig().get(player.getUniqueId() + ".hp"));
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute " + player.getName() + " minecraft:generic.max_health base set "
+				+ (double) plugin.getConfig().get(player.getUniqueId() + ".hp"));
+		//AttackDamage
+		if(plugin.getConfig().get(player.getUniqueId() + ".ad") == null)
+			plugin.getConfig().set(player.getUniqueId() + ".ad" ,
+				(double) plugin.getConfig().get("defaultAD"));
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute " + player.getName() + " minecraft:generic.attack_damage base set "
+				+ (double) plugin.getConfig().get(player.getUniqueId() + ".ad"));
+		//AttackSpeed
+		if(plugin.getConfig().get(player.getUniqueId() + ".as") == null)
+			plugin.getConfig().set(player.getUniqueId() + ".as" ,
+				(double) plugin.getConfig().get("defaultAS"));
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute " + player.getName() + " minecraft:generic.attack_speed base set "
+			+ (double) plugin.getConfig().get(player.getUniqueId() + ".as"));
+		//KnockbackRes
+		if(plugin.getConfig().get(player.getUniqueId() + ".kr") == null)
+			plugin.getConfig().set(player.getUniqueId() + ".kr" ,
+				(double) plugin.getConfig().get("defaultKR"));
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute " + player.getName() + " minecraft:generic.knockback_resistance base set "
+				+ (double) plugin.getConfig().get(player.getUniqueId() + ".kr"));
+		//MovementSpeed
+		if(plugin.getConfig().get(player.getUniqueId() + ".ms") == null)
+			plugin.getConfig().set(player.getUniqueId() + ".ms" ,
+				(double) plugin.getConfig().get("defaultMS"));
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute " + player.getName() + " minecraft:generic.movement_speed base set "
+				+ (double) plugin.getConfig().get(player.getUniqueId() + ".ms"));
 		plugin.saveConfig();
 	}
 }
